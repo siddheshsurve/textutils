85c3395801b7ccb72caa16c10936e7bb78d77fe2
Siddhesh Surve <sysurve24@gmail.com>
2023-02-18 12:22:18 +0000

    used propTypes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
@@ -25,4 +26,9 @@ export default function Navbar(props) {
         </div>
       </nav>
   )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string,
+    aboutText : PropTypes.string
 }
