ebc8730585f9f7be5176dbcccaf8d7e542d824ad
Siddhesh Surve <sysurve24@gmail.com>
2023-02-18 12:15:27 +0000

    used props for navbar contents

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/#">TextUtils</a>
+          <a className="navbar-brand" href="/#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -14,7 +14,7 @@ export default function Navbar() {
                 <a className="nav-link active" aria-current="page" href="/#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#">About</a>
+                <a className="nav-link" href="/#">{props.aboutText}</a>
               </li>
             </ul>
             <form className="d-flex" role="search">
